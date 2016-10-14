2254ab630b0fd30a8b48498a292534283679cea6
Admin <atirekpothiwala@gmail.com>
2016-10-14 17:43:48 +0530

    Initial commit

---
+++ app/src/main/java/com/alm/paypaldemo/PayPalConfig.java
@@ -0,0 +1,9 @@
+package com.alm.paypaldemo;
+
+/**
+ * Created by Admin on 10/14/2016.
+ */
+
+public class PayPalConfig {
+    public static final String PAYPAL_CLIENT_ID = "AfibM2aQVoui3xflLEWXFLhPZPjZdTiEdRvgEludZtEqzJopzAor9sqHZv21KnCIFGEdlf1jIXjCdjaC";
+}
